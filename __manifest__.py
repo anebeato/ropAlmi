db8c114d9e86fd16a745248a05c4b1beabfc3fe6
anebeato <anebeato@gmail.com>
2024-12-16 10:47:53 +0100

    sdasdas

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. Modulo de peliculas realizado en ALMI
+{
+    'name': 'Modulo de ropAlmi',
+    'version': '1.0',
+    'depends' : ['base','contacts','mail'],
+    'author' : 'Almi',
+    'category': 'RopAlmi',
+    'website': '',
+    'description': 'Modulo para gestionar ropas',
+
+}
